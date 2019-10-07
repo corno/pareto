0784e6127903b46e0c9a9e3aaa0a27fce27a380d
corno <corno@schraverus.com>
2019-10-07 21:23:05 +0200

    fixed namespaces for JSON and fs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export * from "./JSON"
-export * from "./fs"
+import * as fs_raw from "./fs"
+import * as JSON_raw from "./JSON"
+
+export const JSON = {
+    parse: JSON_raw.parse,
+}
+export const fs = {
+    access: fs_raw.access,
+}
 export * from "steroid-promise-core"
