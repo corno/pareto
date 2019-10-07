37393cf9ffee5a15309020988c1318dfa106c70a
corno <corno@schraverus.com>
2019-10-07 17:18:57 +0200

    added error type

--- src/parseJSON.ts
+++ src/parseJSON.ts
@@ -1,15 +1,13 @@
 import { UnsafePromise, wrapUnsafeFunction } from "steroid-promise"
 
-export function parseJSON(data: string, ): UnsafePromise<any, null> {
+export function parseJSON(data: string, ): UnsafePromise<any, SyntaxError> {
     return wrapUnsafeFunction(
         (onError, onSucces) => {
-            let json
             try {
-                json = JSON.parse(data)
+                onSucces(JSON.parse(data))
             } catch (e) {
-                onError(null)
+                onError(e)
             }
-            onSucces(json)
         }
     )
 }
