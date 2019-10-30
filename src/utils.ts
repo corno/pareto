1860ed160f0ee8acf7f039ff33939bd12ead8415
corno <corno@schraverus.com>
2019-10-30 11:08:16 +0100

    added arrayToLookup

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,14 @@
 import { Dictionary} from "./classes/Dictionary"
+import { Lookup} from "./classes/Lookup"
 
 export function arrayToDictionary<Type>(array: Type[], keys: string[]) {
     const dictionary: { [key: string]: Type } = {}
     array.forEach((element, index) => dictionary[keys[index]] = element)
     return new Dictionary<Type>(dictionary)
+}
+
+export function arrayToLookup<Type>(array: Type[], keys: string[]) {
+    const dictionary: { [key: string]: Type } = {}
+    array.forEach((element, index) => dictionary[keys[index]] = element)
+    return new Lookup<Type>(dictionary)
 }
