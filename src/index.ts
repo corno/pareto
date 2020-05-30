7752d7dd06cada8470bc6bda993c97ed17d5a9eb
Corno Schraverus <corno@schraverus.com>
2020-05-30 10:49:34 +0200

    u2l

--- src/index.ts
+++ src/index.ts
@@ -30,9 +30,7 @@ export {
     SafeMutableDictionary,
     UnsafeMutableDictionary,
 
-    mergeArrayOfSafeValues,
-    mergeStreamOfUnsafeValues,
-    mergeArrayOfUnsafeValues,
+    IArray,
 
 
     IUnsafeOnOpenResource,
