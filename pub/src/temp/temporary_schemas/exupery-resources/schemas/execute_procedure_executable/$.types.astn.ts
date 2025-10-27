694d4163e96d3e73e790dce7f7fe1e85fc5e6604
Corno Schraverus <corno@schraverus.com>
2025-10-27 19:53:16 +0100

    .

---
+++ pub/src/temp/temporary_schemas/exupery-resources/schemas/execute_procedure_executable/$.types.astn.ts
@@ -0,0 +1,33 @@
+import * as pd from 'exupery-core-data'
+
+import {
+    types,
+    t,
+    type,
+    n,
+    prop,
+    tstate,
+    text,
+} from "../../../../../shorthands/schema"
+import * as g_ from "../../../../../generated/interface/schemas/schema/data_types/target"
+
+export const $: g_.Types<pd.Source_Location> = types(
+    {
+        "Parameters": type(t.group({
+            "program": prop(t.text_local(text('single line'))),
+            "args": prop(t.list(t.text_local(text('single line')))),
+        })),
+
+        "Errors": type(t.state_group({
+            "failed to spawn": tstate(t.group({
+                "message": prop(t.text_local(text('single line'))),
+            })),
+            "non zero exit code": tstate(t.group({
+                "exitCode": prop(t.number_local(n.integer())),
+                "stderr": prop(t.text_local(text('multi line'))),
+            })),
+        })),
+    }
+)
+
+
