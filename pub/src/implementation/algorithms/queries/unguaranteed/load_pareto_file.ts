bb0017fdde1c30a5897d7033ede4a9af8bac1e8a
Corno Schraverus <corno@schraverus.com>
2025-11-13 20:47:30 +0100

    latest pareto-standard-operations

--- pub/src/implementation/algorithms/queries/unguaranteed/load_pareto_file.ts
+++ pub/src/implementation/algorithms/queries/unguaranteed/load_pareto_file.ts
@@ -7,26 +7,21 @@ import * as _easync from 'exupery-core-async'
 
 //data
 
-import * as _out from "../../../../temp/temp_unmashall_result_types"
-
-import { $$ as op_join_with_separator } from "pareto-standard-operations/dist/implementation/algorithms/operations/impure/text/join_list_of_texts_with_separator"
-
-import { get_directory_path } from "../../operations/impure/tbd/path"
-
-import { $ as load_schema } from "../../../../exceptional/deserializers/load_schema"
-
-import { $$ as q_load_astn_document, Error as Document_Error } from "./load_pareto_document"
-import { Signature } from "../../../../interface/algorithms/queries/unguaranteed/load_pareto_file"
-
+import * as d_out from "../../../../temp/temp_unmashall_result_types"
+import * as d_lpd from "./load_pareto_document"
 import * as d_read_file from "exupery-resources/dist/interface/generated/pareto/schemas/read_file/data_types/source"
 
-export type Error =
-    | ['no file', null]
-    | ['document', Document_Error]
+export namespace d {
+    export type Error =
+        | ['no file', null]
+        | ['document', d_lpd.Error]
 
-export type Parameters = {
-    'file path': string,
+    export type Parameters = {
+        'file path': string,
+    }
 }
+
+import { $$ as q_load_astn_document } from "./load_pareto_document"
 
 export type Resources = {
     'queries': {
@@ -34,26 +29,20 @@ export type Resources = {
     }
 }
 
-export const $$: _easync.Unguaranteed_Query<Parameters, _out.Node, Error, Resources> = (
+import { Signature } from "../../../../interface/algorithms/queries/unguaranteed/load_pareto_file"
+
+export const $$: _easync.Unguaranteed_Query<d.Parameters, d_out.Node, d.Error, Resources> = (
     $p, $r
 ) => {
     const instance_path = $p['file path']
-    const schema_path = op_join_with_separator(
-        get_directory_path($p['file path']).transform(
-            ($) => $,
-            () => _ea.deprecated_panic("could not get directory path"),
-        ),
-        {
-            'separator': "/",
-        }
-    ) + "/astn-schema"
+
     return $r.queries['read file'](
         {
             'path': instance_path,
             'escape spaces in path': true,
         },
         null,
-    ).map_exception_(($): Error => ['no file', null])
+    ).map_exception_(($): d.Error => ['no file', null])
         .then_unguaranteed(($) => q_load_astn_document(
             {
                 'content': $,
