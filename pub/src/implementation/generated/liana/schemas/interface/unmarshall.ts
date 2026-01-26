250af9d4a1de9817a4a8f4cb43816024062e1b51
Corno Schraverus <corno@schraverus.com>
2026-01-26 20:22:57 +0100

    ...

---
+++ pub/src/implementation/generated/liana/schemas/interface/unmarshall.ts
@@ -0,0 +1,9 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/interface/unmarshall"
+export const Imports: t_signatures.Imports = ($,) => _p.unreachable_code_path()
+export const Module: t_signatures.Module = ($,) => _p.unreachable_code_path()
+export const Module_Set: t_signatures.Module_Set = ($,) => _p.unreachable_code_path()
+export const Type_Reference: t_signatures.Type_Reference = ($,) => _p.unreachable_code_path()
+export const Type_Node: t_signatures.Type_Node = ($,) => _p.unreachable_code_path()
---
+++ pub/src/implementation/generated/liana/schemas/typescript_light/unmarshall.ts
@@ -0,0 +1,11 @@
+
+import * as _p from "pareto-core/dist/refiner"
+
+import * as t_signatures from "../../../../../interface/generated/liana/schemas/typescript_light/unmarshall"
+export const Identifier: t_signatures.Identifier = ($,) => _p.unreachable_code_path()
+export const Function_Parameters: t_signatures.Function_Parameters = ($,) => _p.unreachable_code_path()
+export const String_Literal: t_signatures.String_Literal = ($,) => _p.unreachable_code_path()
+export const Type: t_signatures.Type = ($,) => _p.unreachable_code_path()
+export const Expression: t_signatures.Expression = ($,) => _p.unreachable_code_path()
+export const Statements: t_signatures.Statements = ($,) => _p.unreachable_code_path()
+export const Directory: t_signatures.Directory = ($,) => _p.unreachable_code_path()
