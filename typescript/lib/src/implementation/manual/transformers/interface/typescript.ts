d5af7cba787dd0b9e96f2a52a0fc8ad12c834b35
Corno Schraverus <corno@schraverus.com>
2026-07-08 13:40:19 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/interface/typescript.ts
+++ typescript/lib/src/implementation/manual/transformers/interface/typescript.ts
@@ -5,23 +5,11 @@ import p_variables from 'pareto-core/implementation/refiner/specials/variables'
 import p_text_from_list from 'pareto-core/implementation/transformer/specials/text_from_list'
 import p_list_from_text from 'pareto-core/implementation/refiner/specials/list_from_text'
 
+import type * as interface_ from "../../../../interface/declarations/transformers/interface/typescript.js"
+
 import type * as d_in from "../../../../interface/generated/liana/schemas/interface/data/resolved.js"
 import type * as d_out from "../../../../interface/generated/liana/schemas/typescript_light/data.js"
 import type * as d_function from "../../../../interface/data/pareto_to_typescript.js"
-
-export namespace interface_ {
-    export type Package_Set = p_ri.Refiner<
-        d_out.Directory,
-        d_function.Error,
-        d_in.Package_Set
-    >
-    export type Value = p_i.Transformer_With_Parameter<
-        d_in.Value,
-        d_out.Statements,
-        { 'name': string }
-    >
-}
-import * as temp_interface_ from "../../../../interface/declarations/transformers/interface/typescript.js"
 
 //dependencies
 import { Text as s_file_name } from "../text/filename.js"
--- typescript/lib/src/implementation/manual/transformers/text/filename.ts
+++ typescript/lib/src/implementation/manual/transformers/text/filename.ts
@@ -1,19 +1,8 @@
 import * as p_ from 'pareto-core/implementation/transformer'
 import p_list_build_deprecated from 'pareto-core/implementation/refiner/specials/list_build_deprecated'
 import p_list_from_text from 'pareto-core/implementation/refiner/specials/list_from_text'
-import type * as p_i from 'pareto-core/interface/transformer'
 
-//data types
-import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/list_of_characters/data"
-
-export namespace interface_ {
-
-    export type Text = p_i.Transformer<
-        string, d_out.List_of_Characters
-    >
-
-}
-import * as temp_interface_ from "../../../../interface/declarations/transformers/text/filename.js"
+import type * as interface_ from "../../../../interface/declarations/transformers/text/filename.js"
 
 export const Text: interface_.Text = ($) => {
     return p_list_build_deprecated(
